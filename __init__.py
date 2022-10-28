8e00b5447ace20d0e0cbf8c4d2a8efd5f4ca44fe
Jan Vansteenkiste <jan@vstone.eu>
2022-10-28 16:26:56 +0200

    Remove import (not needed)

--- __init__.py
+++ __init__.py
@@ -1,6 +1,2 @@
 """Turn into a proper python module."""
 
-# Works around the fact that this is a module and can be approached
-# as such. However, with EDMC's plugin loading, things
-# can get a little messy and with this little hack, I try to keep my IDE happy.
-from edsmquery import *  # pylint: disable=wildcard-import # noqa: F401, F403
